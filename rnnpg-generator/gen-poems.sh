eb939be8b3bfb8be3afb1398ae7d2cfec6176ff2
阿光 <hanweiguang1@gmail.com>
2017-01-09 18:52:56 -0800

    更新了gitignore
    修改了生成脚本，解决了debug版本下运行出现的问题

--- rnnpg-generator/gen-poems.sh
+++ rnnpg-generator/gen-poems.sh
@@ -4,6 +4,20 @@ keywordsF=../MISC/rand.keywords.30
 outputF=$keywordsF.poems.out
 outstdF=$keywordsF.poems.std
 
+codedir=rnn_poem_postprecessing
+cd $codedir
+if [ ! -d "./bin" ] ; then
+	mkdir bin
+fi
+if [ ! -d "./dist" ] ; then
+	mkdir dist
+fi
+javac -sourcepath ./src -d ./bin ./src/dio/*.java
+javac -sourcepath ./src -d ./bin ./src/rnn_poem/*.java
+jar -cfe dist/rnn_poem_post_precessor.jar rnn_poem.Postprocessor -C bin/ .
+cd ..
+cp $codedir/dist/rnn_poem_post_precessor.jar rnn_poem_post_precessor.jar
+
 ./rnnpg-generator poem_generator.conf $keywordsF.firstSent.txt $outputF
 
 # convert the verbose format to human readable format
